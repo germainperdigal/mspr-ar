09e5f032003bb06c74a4ab2ef90c311ddfa5fd17
Germain PERDIGAL <g.perdigal@ldlc.com>
2022-06-18 11:52:45 +0200

    Base project

---
+++ README.md
@@ -0,0 +1,3 @@
+# Appli flutter AR
+
+Appli flutter AR pour le MSPR EPSI I4.
---
+++ android/app/src/main/kotlin/com/gperdigal/mspr/app_mspr/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.gperdigal.mspr.app_mspr
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
